11a00d5c98d101c122a7a4781a4e0f3c0760a0fa
khanhln <khanhln2907@gmail.com>
2022-09-11 21:07:45 +0200

    added script to evaluate time auto correlation

--- EvaluationScripts/main.m
+++ EvaluationScripts/main.m
@@ -14,8 +14,89 @@ tInterval = [-inf inf];
 close all;
 %plot_flight_mode()
 %plot_attitude()
-plot_pid_controller()
+%plot_pid_controller()
+%analyze_cross_corr_pid(693, 695);
+analyze_cross_corr_seeker(687, 696);
 
+function analyze_cross_corr_pid(t_min_s, t_max_s)
+    global data
+    %global tInterval
+    
+    
+    fRate = get_topic_sample_interval(data.rawDataTable.FILTERED_ATTITUDE_RATE, t_min_s, t_max_s);
+    pPID = get_topic_sample_interval(data.rawDataTable.PID_CONTROLLER.pPID, t_min_s, t_max_s);
+
+    figure("Name", "p Out vs fRate p");
+    [c,lags] = xcorr(fRate.p, pPID.outP, 50);
+    stem(lags,c)
+    
+    %%
+    pPID = get_topic_sample_interval(data.rawDataTable.PID_CONTROLLER.pPID, t_min_s, t_max_s);
+    %dPID = get_topic_sample_interval(data.rawDataTable.PID_CONTROLLER.dPID, t_min_s, t_max_s);
+    %pidTotal = get_topic_sample_interval(data.rawDataTable.PID_CONTROLLER.Total, t_min_s, t_max_s);
+    figure("Name", "pOut, pidTotal");
+    subplot(221);
+    [c,lags] = xcorr(pPID.outP, pPID.Total, 20);
+    stem(lags,c)
+    title("p vs Total")
+    
+    subplot(222);
+    [c,lags] = xcorr(pPID.outD, pPID.Total, 20);
+    stem(lags,c)
+    title("d vs Total")
+
+    subplot(223);
+    [c,lags] = xcorr(pPID.outP, pPID.outD, 20);
+    stem(lags,c)
+    title("p vs d")
+
+    subplot(224);
+    [c,lags] = xcorr(fRate.p, pPID.Total, 20);
+    stem(lags,c)
+    title("fRate vs PID")
+end
+
+function analyze_cross_corr_seeker(t_min_s, t_max_s)
+    global data
+    
+    encSample = data.rawDataTable.TARGET_INFO(:, ["TimePacket", "Phi_Enc", "Theta_Enc", "Psi_Enc"]);
+    encSample.Time = encSample.TimePacket;
+    encSample.TimePacket = [];
+    encSample.Phi = encSample.Phi_Enc;
+    encSample.Theta = encSample.Theta_Enc;
+    encSample.Psi = encSample.Psi_Enc;
+    
+    
+    encAttData = get_topic_sample_interval(encSample, t_min_s, t_max_s);
+    analyzedEncSample = AttitudeSample("Encoder_Seeker", encAttData, 120);    
+
+    att = get_topic_sample_interval(data.rawDataTable.ATTITUDE, t_min_s, t_max_s);
+    
+    figure("Name", "Enc Att vs UAV Att");
+    subplot(2,2,1)
+    [c,lags] = xcorr(att.Phi, analyzedEncSample.data.Phi);
+    stem(lags,c);
+    legend(["Phi"]);
+
+    subplot(2,2,2)
+    plot(att.Time / 1e6, att.Phi, "-o", "DisplayName", "Phi UAV");
+    hold on; grid on;
+    plot(analyzedEncSample.data.Time, -analyzedEncSample.data.Phi, "-o", "DisplayName", "Phi ENC");
+    legend;
+    
+    subplot(2,2,3)
+    [c,lags] = xcorr(att.Theta, analyzedEncSample.data.Theta);
+    stem(lags,c);
+    legend(["Theta"]);
+
+    subplot(2,2,4)
+    plot(att.Time / 1e6, att.Theta, "-o", "DisplayName", "Theta UAV");
+    hold on; grid on;
+    plot(analyzedEncSample.data.Time, analyzedEncSample.data.Theta, "-o", "DisplayName", "Theta ENC");
+    legend;
+
+    FormatFigure(gcf, 12, 12/8, 'MarkerSize', 2.5);
+end
 
 
 function plot_attitude()
