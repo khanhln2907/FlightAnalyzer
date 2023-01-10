05dcdb00112612f252aa08a41d099e1130060264
Nhan Khanh Le <NKlLe@amdc.de>
2023-01-10 11:49:32 +0100

    reorganized folders

--- Parser/JSONDecode.m
+++
@@ -1,6 +0,0 @@
-%% Wrap function to modifiy the string before using matlab jsondecode builtin function
-function outStruct = JSONDecode(txtLine)
-    formartedLine = sprintf('{%s}', txtLine);
-    %outStruct = loadjson(formartedLine); Test other library but still slow
-    outStruct = jsondecode(formartedLine);
-end
